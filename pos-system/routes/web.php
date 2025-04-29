818445f2be0ec16eb1d589fe8880811a3d2360ef
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-04-29 11:59:01 +0530

    updated

--- pos-system/routes/web.php
+++ pos-system/routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::apiResource('orders', OrderController::class);
-Route::get('/orders/{id}/print', [OrderController::class, 'print'])->name('orders.print');
+zz
