854e98f121c3123f6361bb5f9c4496085f7500a0
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-04-30 19:59:45 +0530

    Create ProductController.php

---
+++ pos-system/app/Http/Controllers/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::with('category')->get();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'category_id' => 'required'
+        ]);
+        return Product::create($request->all());
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(['message' => 'Deleted']);
+    }
+}
